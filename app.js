36e22fed1b0043b65bd71702451c53c07553d638
alasgar88 <85016138+alasgar88@users.noreply.github.com>
2022-08-11 13:17:18 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,85 +1,50 @@
 const btn = document.querySelector(".btn");
 let allDivs = document.querySelectorAll(".add");
-
-// get random coordinates for adding div
-const getCoordinates = (divSize, divSizePx) => {
-  // we get values from 0 -100 as our screen width and heigh between 0% and 100%
-  let width = Math.floor(Math.random() * 100);
-  // to avoit overflow
-  if (width + divSize >= 100) {
-    width = 100 - divSize - 1;
-  }
-  let height = Math.floor(Math.random() * 100);
-  // to avoid averflow
-  if (height + divSizePx >= 100) {
-    height = 100 - divSizePx - 1;
-  }
-  console.log({ width, height, divSize, divSizePx });
-  return { width, height };
-};
+const container = document.querySelector(".container");
 
 // create random color generator function
 function randomInteger(max = 256) {
   return Math.floor(Math.random() * max);
 }
 
-// get random div size / maximum div size will be 15% from window width ,minimum div width/height will be 5% from screen width
+// random divSize
 const getRandomDivSize = () => {
-  // this function will return div width/height in %
-  //  we add + 5 to the end of function because div widht or height cant be 0%
-  const result = Math.floor(Math.random() * 10) + 5;
+  // minimum div size 50px,maximum div size 200px
+  const result = Math.floor(Math.random() * 150) + 50;
   return result;
 };
 
-// add div to window
 const drowSquare = () => {
-  // create div ,add class and postion property
   const div = document.createElement("div");
   div.className = "add";
   div.style.position = "absolute";
-
-  // add random  width and height / call getRandomDivSize function
   const divSize = getRandomDivSize();
-  // get px value from window width
-  const windowWidth =
-    (parseInt(window.innerWidth) * parseInt(`${divSize}%`)) / 100;
-  div.style.width = `${windowWidth}px`;
-  div.style.height = `${windowWidth}px`;
-  // get % value from window height
-  const windowHeight = parseInt(window.innerHeight);
-  const percentage = (windowWidth / windowHeight) * 100;
-  console.log(percentage);
-  //  get random color and apply to div as background color
+  div.style.width = `${divSize}px`;
+  div.style.height = `${divSize}px`;
   div.style.background = `rgba(${randomInteger()},${randomInteger()},${randomInteger()})`;
-
-  // get random coordinates and apply to div
-  const { width, height } = getCoordinates(divSize, percentage);
-  div.style.top = `${height}%`;
-  div.style.left = `${width}%`;
+  // get coordinates for div
+  const { width, height } = container.getBoundingClientRect();
+  var from_top = Math.floor(Math.random() * height);
+  var from_left = Math.floor(Math.random() * width);
+  div.style.top = `${
+    from_top + divSize >= height ? from_top - divSize : from_top
+  }px`;
+  div.style.left = `${
+    from_left + divSize >= width ? from_left - divSize : from_left
+  }px`;
 
   // add div to window
-  window.document.body.appendChild(div);
+  container.appendChild(div);
   allDivs = document.querySelectorAll(".add");
-  removeDivEventListener();
+
+  //  eventListener remove Div
+  allDivs.forEach((div) => {
+    div.addEventListener("click", function () {
+      console.log(this);
+      this.remove();
+    });
+  });
 };
 
-// event listener for adding div
-btn.addEventListener(
-  "click",
-  function () {
-    drowSquare();
-  },
-  false
-);
-
-// remove old eventListener before adding new one
-function removeDivEventListener() {
-  function removeDiv() {
-    this.remove();
-  }
-
-  allDivs.forEach((div) => {
-    div.removeEventListener("click", removeDiv);
-    div.addEventListener("click", removeDiv);
-  });
-}
+//  eventListener add Div
+btn.addEventListener("click", drowSquare);
